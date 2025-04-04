e42e49854904bd94beff63c9e9a8cc90a3163b1f
Whelve.Autism <2303085802@qq.com>
2025-04-04 10:53:45 +0800

    Add files via upload
    version 4.5.2
    进一步优化了注释，尤其是 ScannerInput 类的注释。

--- song-store-assignment-1-2/src/utils/ScannerInput.java
+++ song-store-assignment-1-2/src/utils/ScannerInput.java
@@ -3,26 +3,27 @@ package utils;
 import java.util.Scanner;
 
 /**
- * This class provides methods for the robust handling of I/O using Scanner.
- * It creates a new Scanner object for each read from the user, thereby
- * eliminating the Scanner bug (where the buffers don't flush correctly after an int read).
- *
- * The methods also parse the numeric data entered to ensure it is correct. If it isn't correct,
- * the user is prompted to enter it again.
+ * 此类用于从用户输入中读取数据，并进行验证。
+ * This class is used to read data from the user and perform validation.
  *
  * @author Siobhan Drohan, Mairead Meagher, Siobhan Roche
  * @version 1.0
- *
+ * @since version 1.0
  */
-
 public class ScannerInput {
 
     /**
-     * Read an int from the user.  If the entered data isn't actually an int,
-     * the user is prompted again to enter the int.
+     * 读取一个整数，并验证输入是否为整数。
+     * Read a single integer from the user and validate that the entered data is actually an integer.
      *
-     * @param prompt  The information printed to the console for the user to read
-     * @return The number read from the user and verified as an int.
+     * @param prompt 在控制台打印的信息。
+     *               The information printed to the console for the user to read.
+     * @return 从用户那里读取并验证为 int 类型的数字。
+     *         The number read from the user and verified as an int.
+     * @exception NumberFormatException 如果用户输入无法解析为整数，则抛出此异常。
+     *                                  If the user input cannot be parsed as an integer, throw this exception.
+     * @author Siobhan Drohan, Mairead Meagher, Siobhan Roche
+     * @since version 1.0
      */
     public static int readNextInt(String prompt) {
         do {
@@ -44,11 +45,17 @@ public class ScannerInput {
     }
 
     /**
-     * Read a double from the user.  If the entered data isn't actually a double,
-     * the user is prompted again to enter the double.
+     * 读取一个浮点数，并验证输入是否为浮点数。
+     * Read a single floating-point number from the user and validate that the entered data is actually a floating-point number.
      *
-     * @param prompt  The information printed to the console for the user to read
-     * @return The number read from the user and verified as a double.
+     * @param prompt 在控制台打印的信息。
+     *               The information printed to the console for the user to read.
+     * @return 从用户那里读取并验证为 double 类型的数字。
+     *         The number read from the user and verified as an int.
+     * @exception NumberFormatException 如果用户输入无法解析为浮点数，则抛出此异常。
+     *                                  If the user input cannot be parsed as a floating-point number, throw this exception.
+     * @author Siobhan Drohan, Mairead Meagher, Siobhan Roche
+     * @since version 1.0
      */
     public static double readNextDouble(String prompt) {
         do {
@@ -64,10 +71,15 @@ public class ScannerInput {
     }
 
     /**
-     * Read a line of text from the user.  There is no validation done on the entered data.
+     * 读取一个字符串。
+     * Read a single string from the user.
      *
-     * @param prompt  The information printed to the console for the user to read
-     * @return The String read from the user.
+     * @param prompt  在控制台打印的信息。
+     *                The information printed to the console for the user to read.
+     * @return 从用户那里读取的字符串。
+     *         The string read from the user.
+     * @author Siobhan Drohan, Mairead Meagher, Siobhan Roche
+     * @since version 1.0
      */
     public static String readNextLine(String prompt) {
         Scanner input = new Scanner(System.in);
@@ -76,10 +88,15 @@ public class ScannerInput {
     }
 
     /**
-     * Read a single character of text from the user.  There is no validation done on the entered data.
+     * 读取字符串中的第一个字符。
+     * Read the first character from a string.
      *
-     * @param prompt  The information printed to the console for the user to read
-     * @return The char read from the user.
+     * @param prompt  在控制台打印的信息。
+     *                The information printed to the console for the user to read.
+     * @return 从用户那里读取的字符。
+     *         The char read from the user.
+     * @author Siobhan Drohan, Mairead Meagher, Siobhan Roche
+     * @since version 1.0
      */
     public static char readNextChar(String prompt) {
         Scanner input = new Scanner(System.in);
@@ -89,5 +106,5 @@ public class ScannerInput {
 }
 /*
  * End of utils.ScannerInput Class.
- * Checked by Fan Xinkang on 2025/04/03.
+ * Checked by Fan Xinkang on 2025/04/04.
  */
